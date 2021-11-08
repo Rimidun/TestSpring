0502c919a8ee24f4dd723800101e02fab7f81328
Andrey <x137@yandex.ru>
2021-11-08 23:41:44 +0300

    update class Main version 2

--- src/main/third/many_to_many/Main.java
+++ src/main/third/many_to_many/Main.java
@@ -20,16 +20,29 @@ public class Main {
             session = factory.getCurrentSession();
             session.beginTransaction();
 
-            Section section1 = new Section("Football");
-            Child child1 = new Child("Nikita", 5);
-            Child child2 = new Child("Andrey", 9);
-            Child child3 = new Child("Artur", 7);
+//            Section section1 = new Section("Football");
+//            Child child1 = new Child("Nikita", 5);
+//            Child child2 = new Child("Andrey", 9);
+//            Child child3 = new Child("Artur", 7);
+//
+//            section1.addChildToSection(child1);
+//            section1.addChildToSection(child2);
+//            section1.addChildToSection(child3);
+//
+//            session.save(section1);
 
-            section1.addChildToSection(child1);
-            section1.addChildToSection(child2);
-            section1.addChildToSection(child3);
+            /********************************** */
 
-            session.save(section1);
+            Child child1 = new Child("Alex", 5);
+            Section section1 = new Section("Dance");
+            Section section2 = new Section("Race");
+            Section section3 = new Section("Run");
+
+            child1.addSectionToChild(section1);
+            child1.addSectionToChild(section2);
+            child1.addSectionToChild(section3);
+
+            session.save(child1);
 
 
             session.getTransaction().commit();
